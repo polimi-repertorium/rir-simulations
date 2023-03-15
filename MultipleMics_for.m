d842819663c9a3f35e6aac134fcaf0332c72ecb0
francescaronchini <ronchinifrancesca8@gmail.com>
2023-03-15 16:19:39 +0100

    Adding script for parallel computing

--- MultipleMics_for.m
+++ MultipleMics_for.m
@@ -47,20 +47,12 @@ fprintf("Nsample: %d", nsample)
 
 
 %% setting ULA mics (always same value, need to be changed)
-ULA_n_mic = 3;
+ULA_n_mic = 64;
 ULA=[config.ULA.position(1)*ones(ULA_n_mic,1), config.ULA.position(2)*ones(ULA_n_mic,1), config.ULA.position(3)*ones(ULA_n_mic,1)];
 
-
-c = config.c;
-procFs = config.procFs;
-source_location = config.source.location;
-room_dimension = config.room.dimension;
-room_beta = config.room.beta;
-room_order = config.room.order;
-
 t_ULA_start = tic;
-parfor i = 1:ULA_n_mic
-    h_rir = rir_generator(c, procFs, mic_pos, source_location, room_dimension, room_beta, nsample, 'omnidirectional', room_order, 3, [0 0], false);
+for i = 1:ULA_n_mic
+    h_rir = rir_generator(config.c, config.procFs, mic_pos, config.source.location, config.room.dimension, config.room.beta, nsample, 'omnidirectional', config.room.order, 3, [0 0], false);
 end
 t_ULA_end = toc(t_ULA_start);
 disp(t_ULA_end)
@@ -69,37 +61,30 @@ disp(t_ULA_end)
 SPHERE_mic_max = 2;
 results = zeros(SPHERE_mic_max,2);
 
-sphere_location = config.sphere.location;
-sphere_type = config.sphere.type;
-sphere_radius = config.sphere.radius;
-N_harm = config.N_harm;
-K = config.K;
-src_type = config.source.src_type;
-
 for n_mic = 1:SPHERE_mic_max
-    t_smir_start = tic;
     results(n_mic, 1) = n_mic;
-    parfor sphere = 1:n_mic
+    t_smir_start = tic;
+    for sphere = 1:n_mic
         fprintf("n_mic: %d; sphere: %d\n", n_mic, sphere)
-        [h_smir, H_smir, beta_hat] = smir_generator(c, ...
-            procFs, ...
-            sphere_location, ...
-            source_location, ...
-            room_dimension, ...
-            room_beta, ...
-            sphere_type, ...
-            sphere_radius, ...
+        [h_smir, H_smir, beta_hat] = smir_generator(config.c, ...
+            config.procFs, ...
+            config.sphere.location, ...
+            config.source.location, ...
+            config.room.dimension, ...
+            config.room.beta, ...
+            config.sphere.type, ...
+            config.sphere.radius, ...
             mic, ...
-            N_harm, ...
+            config.N_harm, ...
             nsample, ...
-            K, ...
-            room_order, ...
+            config.K, ...
+            config.room.order, ...
             0, ...
             0, ...
-            src_type, ...
+            config.source.src_type, ...
             src_ang);
-    end  
-    t_smir_end = toc(t_smir_start);
+    end
+    t_smir_end = toc(t_smir_start); 
     % timing
     results(n_mic,2) = t_smir_end + t_ULA_end;
 end
@@ -107,7 +92,7 @@ end
 
 %% save csv file
 res_table = array2table(results, "VariableNames",["Multiple_mics","Time"]);
-full_file_path = fullfile(csv_folder, strcat('multiple_mics_par', room, '.csv'));
+full_file_path = fullfile(csv_folder, strcat('multiple_mics_t', room, '.csv'));
 writetable(res_table,full_file_path);
 
 %% plot
@@ -119,5 +104,5 @@ xlabel("Multiple mics")
 ylabel("Time (s)")
 set(gca,'xtick',1:20)
 ylim([160 250])
-filename_path = fullfile(plot_folder, strcat('multiple_mics_par', room, '.png'));
+filename_path = fullfile(plot_folder, strcat('multiple_mics_t', room, '.png'));
 saveas(gcf,filename_path)
