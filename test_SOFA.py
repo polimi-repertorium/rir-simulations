77cfed3d737a005c9c6a9efc2f3d6a53f2ae8134
francescaronchini <ronchinifrancesca8@gmail.com>
2023-04-27 17:49:05 +0200

    First draft reading SOFA file from python

--- test_SOFA.py
+++ test_SOFA.py
@@ -4,20 +4,21 @@ from pathlib import Path
 
 if __name__ == '__main__':
 
-    # make dir where to save RIRs plot
-    RIR_plots_folder = Path("/nas/home/fronchini/rir-simulations/RIRs_plot")
+    # make dir to save RIRs plot
+    cwd_folder = Path.cwd()
+    RIR_plots_folder = cwd_folder / "RIR_plots"
     Path(RIR_plots_folder).mkdir(exist_ok=True)
-    
-    # path of the SOFA file to read
-    filepath = Path("/nas/home/fronchini/rir-simulations/src/lib/data/SOFA/GeneralFIRtest.sofa")
 
+    # path of the SOFA file to read
+    filename = "GeneralFIRtest.sofa"
+    filepath = cwd_folder / "src/lib/data/SOFA/" / filename
     # read the SOFA file
     audio, source_coordinates, receiver_coordinates = pf.io.read_sofa(str(filepath), verify=True)
+    
+    # RIR plot
     pf.plot.time(audio)
-
     fig = plt.figure()
     ax1 = pf.plot.time(audio)
-
     # Save RIRs plot 
     RIR_fig_filename = RIR_plots_folder / "SOFA_test.png"
     fig.savefig(RIR_fig_filename)
