1023867bf29b8c1de7f010ab5c6f990f0d67825b
francescaronchini <ronchinifrancesca8@gmail.com>
2023-05-11 14:56:00 +0200

    small fixes

--- src/lib/utilsContainer.m
+++ src/lib/utilsContainer.m
@@ -1,36 +1,5 @@
 classdef utilsContainer
     methods
-        function config = read_json(~, fname)
-            fid = fopen(fname); 
-            raw = fread(fid,inf); 
-            str = char(raw'); 
-            fclose(fid);
-            config = jsondecode(str);
-        end
-
-        function [] = write_json(~, json_data, fname)
-            if size(json_data.room.dimension, 1) == 1
-                json_data.room.dimension = {json_data.room.dimension};
-            end
-
-            if size(json_data.source.position, 1) == 1
-                json_data.source.position = {json_data.source.position};
-            end
-
-            if size(json_data.ULA.position, 1) == 1
-                json_data.ULA.position = {json_data.ULA.position};
-            end
-
-            if size(json_data.SMA.position, 1) == 1
-                json_data.ULA.position = {json_data.ULA.position};
-            end
-
-            json_data = jsonencode(json_data);
-            fid = fopen(fname, 'w');
-            fprintf(fid, '%s', json_data);
-            fclose(fid);
-        end
-
         function freq = room_mode(~, c, room_dim, Nx, Ny, Nz)
             freq = c/2 * sqrt(((Nx/room_dim(1))^2) + ((Ny/room_dim(2))^2) + ((Nz/room_dim(3))^2));
             display(freq)
--- src/lib/writeSOFA.m
+++ src/lib/writeSOFA.m
@@ -14,19 +14,16 @@ function [] = writeSOFA(rir, nsample, mic_pos, receiver_pos, full_path_filename)
     % Fill data with data
     M=1; % only one measurement
     Obj.Data.IR = zeros(M,R,N); % data.IR must be [M R N]
-    disp(size(Obj.Data.IR))
     Obj.Data.Delay = zeros(1, R);
     Obj.ReceiverPosition_Type = 'cartesian';
     Obj.EmitterPosition_Type = 'cartesian';
     Obj.ReceiverPosition_Units = 'metre, metre, metre';
     Obj.EmitterPosition_Units = 'metre, metre, metre';
     
-
     Obj.Data.IR(1,:,:)=IR(:, :);
     Obj.ListenerPosition(:,:)=mic_pos(:, :);
     Obj.ReceiverPosition=receiver_pos(:, :);
-    
-
+   
     
     %% Update dimensions
     Obj=SOFAupdateDimensions(Obj);
